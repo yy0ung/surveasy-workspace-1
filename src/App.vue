81cfe07f04cbc60e698d9f021167c6948397f40f
young <syoh0313@yonsei.ac.kr>
2022-01-19 17:15:47 +0900

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app" :class="{backactive: this.$store.state.showModal}">
   <div class="cookies-modal" v-if="show==true">
-    <p class="cookies-title">Surveasy 홈페이지는 Safari 브라우저를 지원하지 않습니다.</p>
+    <p class="cookies-title">Surveasy 홈페이지는 Safari 브라우저를 지원하지 않습니다. <span id="small-f">* Chrome, Edge, Whale 권장.</span></p>
     <p class="cookies-none" @click="setCookie">7일동안 숨기기</p>
   </div>
   <div id="nav">
@@ -291,7 +291,9 @@ body {
   text-decoration: underline;
 }
 
-
+#small-f{
+  font-size: 0.7rem;
+}
 
 
 #nav a {
