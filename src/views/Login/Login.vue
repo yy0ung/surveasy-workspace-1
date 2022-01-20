46467153661bbfb4f5365531d5dcabeba0254f73
seoyamin <seoyamin@ewhain.net>
2022-01-20 10:20:35 +0900

    previous user notice

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -8,12 +8,17 @@
     <li>
       <input type="password" placeholder="비밀번호" v-model="password" @keyup.enter="signIn">
     </li>
+    
     <li>
       <div class="error">{{ error }}</div>
     </li>
     <li>
+      <div class="lgn-notice">{{ notice }}</div>
+    </li>
+    <li>
       <button type="submit" class="loginbtn" @click="signIn">로그인하기</button>
     </li>
+    
     
   </div>
   <div class="sublogin">
@@ -39,7 +44,8 @@ export default {
     return{
       email:'',
       password:'', 
-      error: ''
+      error: '',
+      notice: '',
     }
   },
   mounted() {
@@ -171,7 +177,8 @@ export default {
           }
 
           if(error.code=="auth/user-not-found") {
-            this.error = "가입되지 않은 이메일 주소입니다."
+            this.error = "가입되지 않은 이메일 주소입니다." 
+            this.notice = "웹사이트 리뉴얼로 인해, 기존 회원분들도 새로운 회원가입이 필요합니다."
           }
 
           if(error.code=="auth/wrong-password") {
@@ -227,6 +234,10 @@ export default {
   outline: none;
   border: 1.5px solid #0AAB00;
   
+}
+.lgn-notice {
+  font-size: 10px;
+  color: rgb(225, 5, 5);
 }
 .error {
   color: rgb(225, 5, 5);
