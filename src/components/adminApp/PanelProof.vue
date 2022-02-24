3343e8c577bef36f74da7bd8b366adacf315b7ba
young <syoh0313@yonsei.ac.kr>
2022-02-24 10:12:07 +0900

    add

--- src/components/adminApp/PanelProof.vue
+++ src/components/adminApp/PanelProof.vue
@@ -1,18 +1,12 @@
 <template>
 <div id="admin-panelProof">
 <h4>설문 인증 {{id}}</h4>
-<<<<<<< HEAD
 <h4>{{nowHeadCount}} / {{headCount}}</h4>
 
 <div class="wrong-notification" v-for="token in wrongTokens" :key="token">
   <ol>
     <li id="token"><input type="checkbox">{{token}}</li>
-=======
 
-<div class="wrong-notification" v-for="token in wrongTokens" :key="token">
-  <ol>
-    <li>{{token}}</li>
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
   </ol>
 </div>
 <div>
@@ -22,42 +16,18 @@
 
 <span v-for="item in urlList" :key="(item)">
   <img :src="item[0]" class="proof-image">
-<<<<<<< HEAD
+
   <input type="checkbox" @click="cancelProof(item[1])">
   <span class="fileName" :class="{active: changeColor.indexOf(item[1].substring(38))!=-1}">{{item[1].substring(38)}}</span>
   
   
 </span>
-=======
-  <!-- 버튼 눌렀을 때 기능 구현 -->
-  <input type="checkbox" @click="cancelProof(item[1],item[0])">{{item[1].substring(38)}}
-  
-</span>
-<!-- <div class="proof-dialog" v-if="showDialog">
-    <h4>인증 사진 지우기</h4>
-    <div class="wrong-img">
-      <img :src="imgCheck">
-      <p>이용자 uid__업로드 날짜시간 : {{wrongProofInfo}}</p>
-    </div>
-    <button @click="showDialog=false">창 닫기</button>
-    <button @click="cancelProof(wrongProofInfo)">취소하기</button>
-</div> -->
-<!-- <span v-for="item in imageInfo" :key="item">
-  <span id="info-text">{{item}}</span>
-</span> -->
-
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
 </div> 
 </template>
 
 <script>
 import { getStorage, ref, getDownloadURL, listAll, list, deleteObject } from "firebase/storage"
-<<<<<<< HEAD
 import { getDocs, getDoc, setDoc, updateDoc, doc, collection, arrayUnion, deleteDoc, arrayRemove } from 'firebase/firestore';
-=======
-import { getDocs, getDoc, setDoc, updateDoc, doc, collection, arrayUnion } from 'firebase/firestore';
-import { getMessaging, onMessage } from "firebase/messaging";
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
 export default {
   data() {
     return {
@@ -66,7 +36,6 @@ export default {
       imgCheck : "",
       wrongProofInfo : "",
       userUid : "",
-<<<<<<< HEAD
       wrongProofUserUid : [],
       userInfo : [],
       wrongTokens : [],
@@ -74,12 +43,6 @@ export default {
       changeColor : [],
       headCount : 0,
       nowHeadCount : 0
-=======
-      userInfo : [],
-      wrongTokens : [],
-      wrongUrl : [],
-      selected : false
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       
       //imageInfo : []
     }
@@ -88,38 +51,14 @@ export default {
     this.downLoad()
   },
   methods: {
-<<<<<<< HEAD
     async cancelProof(info){
-=======
-    async cancelProof(info,url){
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       this.wrongProofInfo = info
       var start = this.wrongProofInfo.indexOf("/")
       var end = this.wrongProofInfo.indexOf("_")
       this.userUid = this.wrongProofInfo.substring(start+1,end)
-<<<<<<< HEAD
       this.wrongProofUserUid.push(this.userUid)
       const db = this.$store.state.db
       this.fbSaveWrongInfo.push(info.substring(38))
-
-=======
-      const db = this.$store.state.db
-      this.wrongUrl.push(url)
-      
-      // const imgRef = ref(storage,path)
-      // deleteObject(imgRef).then(() => {
-      //   if(confirm("삭제 성공")){
-      //     this.showDialog=false
-      //     this.$router.go()
-      //   }
-
-      // }).catch((error) => {
-      //   window.alert("삭제 실패")
-      // })
-
-      
-    
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       const docSnap = await getDocs(collection(db,"AndroidUser"))
       docSnap.forEach((doc) => {
         if(doc.id == this.userUid){
@@ -138,7 +77,6 @@ export default {
       const db = this.$store.state.db
       const docRef = doc(db, "WrongProof", this.id.toString())
       const docSnap = await getDoc(docRef)
-<<<<<<< HEAD
       const docRef2 = doc(db, "AndroidSurvey", this.id.toString())
       const docSnap2 = await getDoc(docRef2)
       this.headCount = docSnap2.data().requiredHeadCount
@@ -147,22 +85,12 @@ export default {
       if(docSnap.exists()){
         wrongFileName.push(docSnap.data().fileName)
         console.log("doc",wrongFileName[0])
-=======
-      var wrong = []
-      if(docSnap.exists()){
-        wrong.push(docSnap.data())
-        console.log(wrong)
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       }
       
       const listRef = ref(storage, this.id+"/")
       await listAll(listRef)
       .then((res) => {
-<<<<<<< HEAD
         //window.alert("불러오는 중")
-=======
-        window.alert("불러오는 중")
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       res.prefixes.forEach((folderRef) => {
        //console.log(folderRef)
      });
@@ -172,7 +100,7 @@ export default {
         getDownloadURL(path).then((url) => {
         var list = [url,itemRef._location.path_]
         this.urlList.push(list)
-<<<<<<< HEAD
+
         //console.log(list)
         var i =0
         //왜 안되는지 모르겠는데 숫자가 크면 됨.
@@ -191,49 +119,33 @@ export default {
         })
       })
     }).catch((error) => {
-=======
-  
-        })
-        
-      })
-      
-    }).catch((error) => {
-      
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
     });
       },
     
   async doneCheck(){
     const db = this.$store.state.db
     const docRef = doc(db, "WrongProof", this.id.toString())
-<<<<<<< HEAD
+
     const docRef2 = doc(db, "AndroidSurvey", this.id.toString())
     const docSnap2 = await getDoc(docRef2)
     
     console.log(docSnap2.data().respondedPanel)
   
-=======
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
     const docSnap = await getDoc(docRef)
     var i =0
     if(docSnap.exists()){
       console.log("yes")
       var i =0
-<<<<<<< HEAD
+
       while(i<this.fbSaveWrongInfo.length){
       await updateDoc(docRef,{
         fileName : arrayUnion(this.fbSaveWrongInfo[i])
-=======
-      while(i<this.wrongUrl.length){
-      await updateDoc(docRef,{
-        url : arrayUnion(this.wrongUrl[i])
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
       })
       i++
     }
     }else{
       await setDoc(docRef,{
-<<<<<<< HEAD
+
         fileName : this.fbSaveWrongInfo
       })
     }
@@ -257,12 +169,6 @@ export default {
     
     }
     window.alert("처리 완료")
-=======
-        url : this.wrongUrl
-      })
-    }
-
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
     this.$router.go()
     
       
@@ -293,7 +199,7 @@ export default {
   top: 50px;
   background: #c5c4c4;
 }
-<<<<<<< HEAD
+
 .fileName.active{
   color: red;
 }
@@ -301,11 +207,4 @@ export default {
   font-size: 13px;
 }
 
-=======
-.proof-image.active{
-  border: red solid 1px;
-}
-
-
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
 </style>
--- src/components/adminApp/PanelSent.vue
+++ src/components/adminApp/PanelSent.vue
@@ -7,15 +7,14 @@
     <th>name</th>
     <th>email</th>
     <th>phoneNumber</th>
-<<<<<<< HEAD
+
     <th>responded survey ID</th>
-=======
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
+
     <th>sent</th>
     <th>sentDone</th>
   </tr>
 
-<<<<<<< HEAD
+
     <tr v-for="item in (this.$store.state.adminAppUserData)" :key="item.info[0].uid" class="list" :class="{active:item.info[0].reward_current == 0}">
     <td>{{item.info[0].name}}</td>
     <td>{{item.info[0].email}}</td>
@@ -24,15 +23,7 @@
     <td>{{item.info[0].reward_current}}</td>
     <td><input type="checkbox" id="done"></td>
     <td><button @click="sentFin(item.info[0].uid, item.respondedSurvey)">정산 완료</button></td>
-=======
-    <tr v-for="item in (this.$store.state.adminAppUserData)" :key="item.uid" class="list" :class="{active:item.reward_current == 0}">
-    <td>{{item.name}}</td>
-    <td>{{item.email}}</td>
-    <td>{{item.phoneNumber}}</td>
-    <td>{{item.reward_current}}</td>
-    <td><input type="checkbox" id="done"></td>
-    <td><button @click="clearCurrent(item.uid)">정산 완료</button></td>
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
+
   </tr>
  
 </div>
@@ -47,7 +38,7 @@ export default {
     }
   },
   created() {
-<<<<<<< HEAD
+
     this.$store.state.adminAppUserData = []
     this.fetchPanelInfo()
   },
@@ -71,22 +62,6 @@ export default {
       }
     },
 
-=======
-    this.$store.state.adminAppUserSentData = []
-    this.fetchPanelSentInfo()
-  },
-  methods: {
-    async fetchPanelSentInfo(){
-      const db = this.$store.state.db
-      const adminAppUserSentData = this.$store.state.adminAppUserSentData
-      
-      const querySnapshot = await getDocs(collection(db,"AndroidUser"))
-      querySnapshot.forEach((doc) => {
-        adminAppUserSentData.push(doc.data())
-      })
-      
-    },
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
     async clearCurrent(Id){
       const db = this.$store.state.db
       const rewardRef = doc(db, "AndroidUser", Id.toString())
@@ -95,7 +70,7 @@ export default {
       })
       
       window.alert('정산 완료')
-<<<<<<< HEAD
+
     },
 
     async fetchPanelInfo(){
@@ -130,9 +105,6 @@ export default {
       adminAppUserData.push(panel)
     }
 
-=======
-    }
->>>>>>> 8ec3aa93fe6ffbc5778297b2f339519b303051d1
     
   },
 }
