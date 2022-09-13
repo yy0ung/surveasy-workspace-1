533b555526b1d1e87f62722af9b6ec8366ae01d3
Minyong SHIN <mustgooo@gmail.com>
2022-09-13 15:41:43 +0900

    이미지수정, 버튼 라우터로 변경

--- src/components/Home/HomeContents.vue
+++ src/components/Home/HomeContents.vue
@@ -13,7 +13,7 @@
               <h3 class="mb-3 mt-3">설문 응답 수집</h3>
               <p class="mb-0 detail-text">한 번에, 손쉽게 설문 응답을 모아보세요. 설문 링크만 올리면 서베이지의 패널이 빠르게 설문에 응답해드립니다.</p>
               <div class="service-button">
-              <button class="btn btn-primary mt-4">더 알아보기</button>
+              <button class="btn btn-primary mt-4"><router-link class="font-white" to="/service">더 알아보기</router-link></button>
               </div>
             </div>
             </div>
@@ -27,7 +27,7 @@
               <h3 class="mb-3 mt-3">인터뷰이 모집</h3>
               <p class="mb-0 detail-text">인터뷰를 해야할 땐, 인터뷰이 모집 서비스를 이용해보세요. 원하는 인터뷰이를, 원하는 일정에 맞게 구해드립니다.</p>
               <div class="service-button">
-              <button class="btn btn-primary mt-4">더 알아보기</button>
+              <button class="btn btn-primary mt-4"><router-link class="font-white" to="/service">더 알아보기</router-link></button>
               </div>
               </div>
             </div>
@@ -41,7 +41,7 @@
               <h3 class="mb-3 mt-3">베타테스터 모집</h3>
                 <p class="mb-0 detail-text">앱이나 웹사이트 출시를 앞두고 계신가요? 정식 출시에 앞서, 베타테스터를 모집해 오류는 없는지 미리 확인해보세요!</p>
                 <div class="service-button">
-                <button class="btn btn-primary mt-4">더 알아보기</button>
+                <button class="btn btn-primary mt-4"><router-link class="font-white" to="/service">더 알아보기</router-link></button>
                 </div>
             </div>
             </div>
@@ -69,5 +69,9 @@ export default {
 
 .service-button {
   text-align: right;
+}
+
+.font-white {
+  color : white;
 }
 </style>
