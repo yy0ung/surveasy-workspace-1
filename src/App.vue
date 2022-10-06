68db64a3da9b8fd6c2fc9de4067f850d7852f3b5
seoyamin <seoyamin@ewhain.net>
2022-10-06 15:45:51 +0900

    docs : feedbank 추가, 이메일 변경

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,7 @@
     <div class="footer-contents">
       <p>서베이지 Surveasy</p>
       <p>상호 : 서베이지</p>
-      <p>이메일 : surveasy2019@yonsei.ac.kr</p>
+      <p>이메일 : official@gosurveasy.com</p>
       <p><router-link :to="{name: 'Term1'}" target="_blank">이용약관</router-link> | 
     <router-link :to="{name: 'Term2'}" target="_blank">개인정보처리방침</router-link></p>
   </div>
